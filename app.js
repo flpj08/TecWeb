d06b8b078c3ec0eed96e189b6b82409262f78819
Filipe J <flpj08@gmail.com>
2017-10-16 01:51:13 -0200

    Inclusão das pastas publicas. A pasta html foi exluida, seu conteúdo (index.html) foi movido para o curriculo.handlebars

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//Body Parser Muddleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('curriculo');
 });
 
 app.listen(8080, () => console.log('Server Iniciou...'));
